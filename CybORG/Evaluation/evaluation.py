b5a71c4cf156bf450a0bb3ffdb63fc0340a10bd4
Maxwell Standen <maxwell.standen@gmail.com>
2023-04-12 18:20:15 +0930

    Uploading the version of the evaluation.py file we used to validate the results including the seed of 523681

--- CybORG/Evaluation/evaluation.py
+++ CybORG/Evaluation/evaluation.py
@@ -17,7 +17,7 @@ def run_evaluation(name, team, name_of_agent, max_eps, write_to_file=True):
     scenario = 'Scenario3'
 
     sg = DroneSwarmScenarioGenerator()
-    cyborg = CybORG(sg, 'sim')
+    cyborg = CybORG(sg, 'sim', seed=523681)
     wrapped_cyborg = wrap(cyborg)
 
     print(f'Using agents {agents}, if this is incorrect please update the code to load in your agent')
@@ -31,6 +31,7 @@ def run_evaluation(name, team, name_of_agent, max_eps, write_to_file=True):
     total_reward = []
     actions_log = []
     obs_log = []
+    total_steps = 0
     for i in range(max_eps):
         observations = wrapped_cyborg.reset()
         action_spaces = wrapped_cyborg.action_spaces
@@ -40,11 +41,12 @@ def run_evaluation(name, team, name_of_agent, max_eps, write_to_file=True):
         # cyborg.env.env.tracker.render()
         count = 0
         for j in range(500):
+            total_steps += 1
             actions = {agent_name: agent.get_action(observations[agent_name], action_spaces[agent_name]) for agent_name, agent in agents.items() if agent_name in wrapped_cyborg.agents}
             observations, rew, done, info = wrapped_cyborg.step(actions)
+            r.append(mean(rew.values()))
             if all(done.values()):
                 break
-            r.append(mean(rew.values()))
             if write_to_file:
                 #a.append({agent_name: str(cyborg.get_last_action(agent_name)) for agent_name in wrapped_cyborg.agents})
                 a.append({agent_name: wrapped_cyborg.get_action_space(agent_name)[actions[agent_name]] for agent_name in actions.keys()})
@@ -56,7 +58,7 @@ def run_evaluation(name, team, name_of_agent, max_eps, write_to_file=True):
     end=datetime.now()
     difference = end-start
     print(f'Average reward is: {mean(total_reward)} with a standard deviation of {stdev(total_reward)}')
-    print(f'file took {difference} amount of time to finish evaluation')
+    print(f'file took {difference} amount of time to finish evaluation with {total_steps} steps')
     if write_to_file:
         with open(file_name+'_summary.txt', 'w') as data:
             data.write(f'CybORG v{cyborg_version}, {scenario}\n')
